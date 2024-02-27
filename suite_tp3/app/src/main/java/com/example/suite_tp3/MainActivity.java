c86a167258f646ebba2794f53defbdf65a7411cb
Houssem Haddad <houssem.haddad02@gmail.com>
2024-02-27 14:19:20 +0100

    tp4

--- suite_tp3/app/src/main/java/com/example/suite_tp3/MainActivity.java
+++ suite_tp3/app/src/main/java/com/example/suite_tp3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.suite_tp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent =getIntent();
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String uri_string ="URI: " + uri;
+            TextView textView = findViewById(R.id.textView);
+            textView.setText(uri_string);
+        }
     }
 }
