373b3c0646219efb8892cb26b5cd71d2b68d35af
Houssem Haddad <houssem.haddad02@gmail.com>
2024-02-28 23:11:43 +0100

    .

--- ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -5,14 +5,20 @@ import static androidx.core.app.ShareCompat.*;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void startActivities(Intent intent) {
-
-    }
+    private BreakIterator mWebsiteEditText;
+    private BreakIterator mLocationEditText;
+    private BreakIterator mShareTextEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +26,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
         Uri uri = intent.getData();
-        if(uri != null) {
+        if( uri != null) {
             String uri_string = getString(R.string.uri_label) + uri.toString();
             TextView textView = findViewById(R.id.text_uri_message);
         }
     }
-
     public void openWebsite(View view) {
         String url = mWebsiteEditText.getText().toString();
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivities(intent);
+            startActivities(new Intent[]{intent});
         } else {
-            log.d("implicite intent ", "cant handle this intent");
+            Log.d("implicite intent ", "cant handle this intent");;
         }
     }
-
     public void openLocation(View view) {
-        String loc = mLocationEditText.getText().toString()
+        String loc = mLocationEditText.getText().toString();
         Uri addressUri = Uri.parse("geo: 0,0?q=" + loc);
         Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -49,11 +53,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public void shareText(View view){
         String txt = mShareTextEditText.getText().toString();
         String mimeType="text/plain";
-        ShareCompat.IntentBuilder;
+        ShareCompat.IntentBuilder
             .from(this)
             .setType(mimeType)
             .setChooserTitle(R.string.share_text_with)
