8b5198aeadd414a30b705fd6f933eff5e5159c8d
Houssem Haddad <houssem.haddad02@gmail.com>
2024-02-28 22:42:45 +0100

    .

--- ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -1,26 +1,63 @@
 package com.example.implicitintents;
 
+import static androidx.core.app.ShareCompat.*;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void startActivities(Intent intent) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if(uri != null) {
+            String uri_string = getString(R.string.uri_label) + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+        }
     }
-    public void openWebsite (View view) {
-// Get the URL text.
-        String url = mWebsiteEdit Text.getText().toString();
-// Parse the URI and create the intent.
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-// Find an activity to handle the intent and start that activity.
-        if (intent.resolve Activity (getPackage Manager()) != null) {
-            startActivity (intent);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivities(intent);
         } else {
-            Log.d( tag: "ImplicitIntents", msg: "Can't handle this intent!");
+            log.d("implicite intent ", "cant handle this intent");
         }
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString()
+        Uri addressUri = Uri.parse("geo: 0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("Implicit Intents", "Can't handle this intent!");
+        }
+    }
+
+
+    public void shareText(View view){
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType="text/plain";
+        ShareCompat.IntentBuilder;
+            .from(this)
+            .setType(mimeType)
+            .setChooserTitle(R.string.share_text_with)
+            .setText(txt)
+            .startChooser();
+    }
 }
